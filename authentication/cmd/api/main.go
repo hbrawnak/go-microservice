7d00a23ece9ec07e423e8d88e7045dcfa095bb27
Habibur Rahman <hbrawnak@gmail.com>
2025-05-03 21:46:57 +0600

    Authentication app and database conection setup

---
+++ authentication/cmd/api/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"authentication/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v5"
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+const port = 80
+
+var count int64
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+	log.Println("Starting authentication service")
+
+	// connect database
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Unable to connect to database")
+	}
+
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	for {
+		db, err := openDB(dsn)
+		if err != nil {
+			log.Println("Error opening database connection")
+			count++
+		} else {
+			log.Println("Connected to database")
+			return db
+		}
+
+		if count > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing off for 2 seconds")
+		time.Sleep(2 * time.Second)
+		continue
+	}
+}
