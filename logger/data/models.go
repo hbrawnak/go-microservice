40dc5704f304400179aa5805cef67527b003fa35
Habibur Rahman <hbrawnak@gmail.com>
2025-05-09 23:44:30 +0600

    log entry method and query for inserting log

---
+++ logger/data/models.go
@@ -0,0 +1,47 @@
+package data
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+	"time"
+)
+
+var client *mongo.Client
+
+type Models struct {
+	LogEntry LogEntry
+}
+
+type LogEntry struct {
+	ID        string    `bson:"_id,omitempty" json:"id,omitempty"`
+	Name      string    `bson:"name" json:"name"`
+	Data      string    `bson:"data" json:"data"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
+}
+
+func (l *LogEntry) Insert(entry LogEntry) error {
+	collection := client.Database("logs").Collection("logs")
+
+	_, err := collection.InsertOne(context.TODO(), LogEntry{
+		Name:      entry.Name,
+		Data:      entry.Data,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	})
+
+	if err != nil {
+		log.Println("Error inserting into logs:", err)
+		return err
+	}
+	return nil
+}
+
+func New(mongo *mongo.Client) Models {
+	client = mongo
+
+	return Models{
+		LogEntry: LogEntry{},
+	}
+}
