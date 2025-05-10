6672c2c16b3e262a2d3df2b53a39e0b5ca4b8505
Habibur Rahman <hbrawnak@gmail.com>
2025-05-10 09:22:47 +0600

    DropCollection method added

--- logger/data/models.go
+++ logger/data/models.go
@@ -94,6 +94,19 @@ func (l *LogEntry) GetOne(id string) (*LogEntry, error) {
 	return &entry, nil
 }
 
+func (l *LogEntry) DropCollection() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	if err := collection.Drop(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func New(mongo *mongo.Client) Models {
 	client = mongo
 
